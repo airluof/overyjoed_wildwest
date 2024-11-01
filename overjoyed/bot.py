01294049a2f4880689402260106ba2a277093848
airluof <etofleks@gmail.com>
2024-11-01 13:26:01 +0300

    Update bot.py

--- overjoyed/bot.py
+++ overjoyed/bot.py
@@ -1,1 +1,20 @@
+import os
+import logging
+from telegram.ext import ApplicationBuilder
+from troll_bot.handlers import get_update_handler, get_forward_handler, get_help_handler
 
+# Установка уровня логирования
+logging.basicConfig(level=logging.DEBUG)
+
+async def main():
+    app = ApplicationBuilder().token(os.getenv('TELEGRAM_TOKEN')).build()
+
+    app.add_handler(get_update_handler())
+    app.add_handler(get_forward_handler())
+    app.add_handler(get_help_handler())
+
+    await app.run_polling()
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
