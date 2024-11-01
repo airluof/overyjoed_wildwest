51abf8647299d29fafed5b47845ab38c1ef2a43d
airluof <etofleks@gmail.com>
2024-11-01 13:30:39 +0300

    Create __init__.py

---
+++ overjoyed/__init__.py
@@ -0,0 +1,12 @@
+# troll_bot/__init__.py
+
+from .handlers import get_update_handler, get_forward_handler, get_help_handler
+from .database import save_message, search_messages
+
+__all__ = [
+    "get_update_handler",
+    "get_forward_handler",
+    "get_help_handler",
+    "save_message",
+    "search_messages",
+]
