4d1a37759e15a66b6ff7bd27d335664c99106cdf
airluof <etofleks@gmail.com>
2024-11-01 13:31:14 +0300

    Update __init__.py

--- overjoyed/__init__.py
+++ overjoyed/__init__.py
@@ -1,4 +1,4 @@
-# troll_bot/__init__.py
+# overjoyed/__init__.py
 
 from .handlers import get_update_handler, get_forward_handler, get_help_handler
 from .database import save_message, search_messages
